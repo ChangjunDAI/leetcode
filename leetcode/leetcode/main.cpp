9ac8f834af2115de48ce3c92450a6c916672a365
ChangjunDAI <65420228+ChangjunDAI@users.noreply.github.com>
2020-10-02 23:08:11 +0200

    first blood
    first project of leetcode

---
+++ leetcode/leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Changjun on 02/10/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
